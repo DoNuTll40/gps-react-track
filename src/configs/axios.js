862fa5c7f072cfa4f9fbcf4130a67d77c7b444f3
DoNuT_ll40 <nuttawoot.ch64@snru.ac.th>
2024-10-29 07:58:18 +0700

    update code

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -4,6 +4,6 @@ import axios from "axios";
 // const ipAddress = window.location.hostname
 
 // axios.defaults.baseURL = `http://${ipAddress}:8000`
-axios.defaults.baseURL = "https://cs-125-personal-projects-64-4jj4yn4hu-donuts-projects.vercel.app"
+axios.defaults.baseURL = "https://cs-125-personal-projects-64.vercel.app"
 
 export default axios;
