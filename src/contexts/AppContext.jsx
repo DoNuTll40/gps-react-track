c48acd635d9e5be096ac90041d7125b44c905c16
DoNuT_ll40 <nuttawoot.ch64@snru.ac.th>
2024-11-01 09:44:05 +0700

    update code

--- src/contexts/AppContext.jsx
+++ src/contexts/AppContext.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import { toast } from "react-toastify";
 import axios from "../configs/axios";
 import { createContext, useEffect, useState } from "react"
 // import LoadingPages from "../components/LoadingPages";
@@ -23,9 +24,29 @@ function AuthContextProvider(props) {
                     headers: { Authorization: `Bearer ${token}`}
                 })
 
-                setUser(rs.data)
+                if(rs.status === 200){
+                    setUser(rs.data)
+                }
+
             } catch (err) {
                 console.log(err)
+                if (err.message === "Network Error") {
+                    return toast.error("ไม่สามารถติดต่อกับเซิฟเวอร์ได้ในขณะนี้", {
+                        theme: "colored",
+                    });
+                  }
+                  toast.error(err.response.data.message, {
+                    theme: "colored",
+                  });
+                  if (err.response.data.message.startsWith("Token verification failed")) {
+                    setUser(null)
+                    localStorage.removeItem('token')
+                  }
+          
+                  if (err.response.data.message.startsWith("TokenExpiredError")) {
+                    setUser(null)
+                    localStorage.removeItem('token')
+                  }
             } finally {
                 setLoading(false)
             }
